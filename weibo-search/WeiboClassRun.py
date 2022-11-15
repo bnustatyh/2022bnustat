e40cbe71882da8b7d5b81d7bfa554326907ee155
欧玮杰 <ouweijie123@outlook.com>
2022-11-15 16:16:54 +0800

    更新微博数据采集实例

---
+++ weibo-search/WeiboClassRun.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# time: 2022/10/1 11:32
+# file: WeiboClassRun.py
+# author: Euclid_Jie
+from WeiboClass import WeiboClass
+
+# 设置参数
+# keyList = ['北师大', '北京师范大学', '北京师范大学统计学院']
+# keyList = ['#北京师范大学120周年校庆#']
+keyList = ['北京师范大学']
+timeBegin = '2021-12-25-0'
+timeEnd = '2022-01-05-0'
+# 运行函数
+WeiboClass(keyList, timeBegin, timeEnd, 3).main_get()
